31e1091d96e058ded4a23e46c121682dcf065081
Sonalc123 <sonal.chauhan1989@gmail.com>
2023-05-06 17:32:02 -0400

    draggable function

--- src/app/users/list.component.ts
+++ src/app/users/list.component.ts
@@ -1,13 +1,14 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { first } from 'rxjs/operators';
+import Sortable from 'sortablejs';
 
 import { AccountService } from '@app/_services';
 
 @Component({ templateUrl: 'list.component.html' })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, AfterViewInit {
     users : any;
     direction : any='desc';
-
+    @ViewChild('table') table: any;
     constructor(private accountService: AccountService) {}
 
     ngOnInit() {
@@ -38,4 +39,14 @@ export class ListComponent implements OnInit {
           })
         this.users=sortedArray;
     }
+
+    ngAfterViewInit() {
+        new Sortable(this.table.nativeElement.getElementsByTagName('tbody')[0], {
+          animation: 150, // Animation speed in milliseconds
+          handle: '.drag-handle', // Selector for the drag handle element
+          onEnd: (event) => {
+            console.log('Moved item with id', event.item.getAttribute('data-id'));
+          }
+        });
+      }
 }
